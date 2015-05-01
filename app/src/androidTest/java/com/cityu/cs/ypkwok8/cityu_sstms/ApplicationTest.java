dfb4b163c3f5061e4a54df6cfed574956779b0b0
unknown <ypkwok8@MMW3420-PRJ71.cs.cityu.edu.hk>
2015-05-02 00:26:02 +0800

    Initial commit

---
+++ app/src/androidTest/java/com/cityu/cs/ypkwok8/cityu_sstms/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.cityu.cs.ypkwok8.cityu_sstms;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
